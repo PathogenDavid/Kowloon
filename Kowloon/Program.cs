7adf259bcb6200e778ef12bb11c2d490cea3fe37
David Maas <contact@pathogenstudios.com>
2019-12-05 01:51:34 -0600

    Created basic Blazor web app for switching between the different test patterns.

---
+++ Kowloon/Program.cs
@@ -0,0 +1,27 @@
+﻿using Kowloon.Core;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Kowloon
+{
+    public static class Program
+    {
+        public static KowloonManager Kowloon { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            using (Kowloon = new KowloonManager())
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseUrls("http://*:80");
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Kowloon/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Kowloon
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+            =>  Configuration = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
